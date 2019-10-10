5d325966fbc1301b33aee9c90892e211b227fb2f
Joanna Chądzyńska <jmalek87@gmail.com>
2019-10-10 10:45:31 +0200

    preparation of routing in app.js,

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/+Home";
+import Login from "./components/+Login";
+import Logout from "./components/+Logout";
+import Register from "./components/+Register";
+import Form from "./components/+Form";
+import NotFound from "./components/+NotFound";
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <div className='app'>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/logowanie' component={Login} />
+          <Route path='/rejestracja' component={Register} />
+          <Route path='/wylogowano' component={Logout} />
+          <Route path='/oddaj-rzeczy' component={Form} />
+          <Route path='*' component={NotFound} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
