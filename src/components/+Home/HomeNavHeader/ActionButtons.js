63748d4131eb83a586f5abf5fdd3d01edd272ad5
Joanna Chądzyńska <jmalek87@gmail.com>
2019-10-10 15:10:00 +0200

    add mixins create variables for media queries

--- src/components/+Home/HomeNavHeader/ActionButtons.js
+++ src/components/+Home/HomeNavHeader/ActionButtons.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ActionButtons = () => {
   return (
     <div className='home__mainBox-btns'>
-      <button>oddaj rzeczy</button>
-      <button>zorganizuj zbiórkę</button>
+      <ul>
+        <li>
+          <Link to='/oddaj-rzeczy'>
+            oddaj <br /> rzeczy
+          </Link>
+        </li>
+        <li>
+          <Link to='/oddaj-rzeczy'>zorganizuj zbiórkę</Link>
+        </li>
+      </ul>
     </div>
   );
 };
