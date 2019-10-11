20ca63a6b57b011fd2a91738e99e07656809ad16
Joanna Chądzyńska <jmalek87@gmail.com>
2019-10-11 14:55:39 +0200

    Create HomeAboutUs and add stylest to it

--- src/components/+Home/HomeAboutUs/HomeAboutUs.js
+++ src/components/+Home/HomeAboutUs/HomeAboutUs.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const HomeAboutUs = () => {
-  return <div>home about us</div>;
+  return (
+    <div className='home__aboutUs'>
+      <div className='home__description-box'>
+        <div className='home__mainBox-title'>
+          <h1>O nas</h1>
+          <div className='decoration'></div>
+        </div>
+        <div className='home__desc'>
+          <p>
+            Nori grape silver beet broccoli kombu beet greens fava bean potato
+            quandong celery. Bunya nuts black-eyed pea prairie turnip leek
+            lentil turnip greens parsnip.
+          </p>
+        </div>
+
+        <div className='home__signature'></div>
+      </div>
+      <div className='home__banner'></div>
+    </div>
+  );
 };
 
 export default HomeAboutUs;
