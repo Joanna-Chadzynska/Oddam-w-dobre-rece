c1cf3d3fb2d32354cba32b356874715e5844e638
Joanna Chądzyńska <jmalek87@gmail.com>
2019-10-14 21:27:23 +0200

    update styles

--- src/components/+Home/HomeContact/ContactForm.js
+++ src/components/+Home/HomeContact/ContactForm.js
@@ -30,7 +30,6 @@ const ContactForm = () => {
 
 	const validate = () => {
 		let isValid = true;
-
 		let nameRegex = /^[a-zA-Z]{1,50}$/;
 		let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		let messageRegex = /^.{120,1000}$/;
