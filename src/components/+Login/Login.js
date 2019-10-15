f64b4fb5976b97dd708bbcda356328035202e74b
Joanna Chądzyńska <jmalek87@gmail.com>
2019-10-15 14:33:05 +0200

    Add login component - layout

--- src/components/+Login/Login.js
+++ src/components/+Login/Login.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { CustomTitle } from "../Layouts";
+import Menu from "../+Home/HomeNavHeader/Menu";
+import UserBar from "../+Home/HomeNavHeader/UserBar";
 
 const Login = () => {
-  return <div>login</div>;
+	return (
+		<div className='login'>
+			<div className='home__nav'>
+				<UserBar />
+				<br />
+				<Menu />
+			</div>
+			<div className='login__container'>
+				<CustomTitle>Zaloguj się</CustomTitle>
+				<form className='form-box'>
+					<div className='inputs-box'>
+						<label htmlFor='email'>Email</label>
+						<input type='email' name='email' id='email' />
+						<br />
+						<br />
+						<label htmlFor='password'>Hasło</label>
+						<input type='password' name='password' id='password' />
+					</div>
+					<div className='buttons-box'>
+						<Link to='/rejestracja'>Załóż konto</Link>
+						<button type='submit'>Zaloguj się</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
