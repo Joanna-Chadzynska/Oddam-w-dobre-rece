47a89e02072e778c506e7035fce2ff2a5fb71e0c
Joanna Chądzyńska <jmalek87@gmail.com>
2019-10-15 14:47:32 +0200

    Add component logout and update styles

--- src/components/+Logout/Logout.js
+++ src/components/+Logout/Logout.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { CustomTitle } from "../Layouts";
+import Menu from "../+Home/HomeNavHeader/Menu";
+import UserBar from "../+Home/HomeNavHeader/UserBar";
 const Logout = () => {
-  return <div>logout</div>;
+	return (
+		<div className='login'>
+			<div className='home__nav'>
+				<UserBar />
+				<br />
+				<Menu />
+			</div>
+			<div className='login__container'>
+				<CustomTitle className='logout__title'>
+					Wylogowanie nastąpiło <br /> pomyślnie!
+				</CustomTitle>
+				<Link to='/' className='logout__link'>
+					Strona główna
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Logout;
